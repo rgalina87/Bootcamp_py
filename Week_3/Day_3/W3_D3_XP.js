696c13eaac41b02c569ce6da2fc980a794434453
rgalina87 <r.galina87@gmail.com>
2020-05-04 16:28:36 +0300

    XP Cond

---
+++ Week_3/Day_3/W3_D3_XP.js
@@ -0,0 +1,13 @@
+let newDog = "Chihuahua"
+
+console.log(newDog.length)
+
+console.log(newDog.toUpperCase)
+
+console.log(newDog.toLowerCase)
+
+if (//What should be true\\) {
+	console.log(‘I LOVE Chihuahua, it’s my favorite dog’)
+} else {
+	console.log(‘I dont care, I prefer CATS’)
+}
