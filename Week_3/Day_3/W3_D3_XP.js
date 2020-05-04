6f24b531e6a5e4c9b29f049f0bcfb0c3e10c803b
rgalina87 <r.galina87@gmail.com>
2020-05-04 16:41:18 +0300

    conditions

--- Week_3/Day_3/W3_D3_XP.js
+++ Week_3/Day_3/W3_D3_XP.js
@@ -6,7 +6,7 @@ console.log(newDog.toUpperCase)
 
 console.log(newDog.toLowerCase)
 
-if (//What should be true\\) {
+if (newDog = "Chihuahua") {
 	console.log(‘I LOVE Chihuahua, it’s my favorite dog’)
 } else {
 	console.log(‘I dont care, I prefer CATS’)
