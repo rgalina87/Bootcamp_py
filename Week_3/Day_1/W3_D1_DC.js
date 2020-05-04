efd3b2c5f9c5b42f4ec62ca0dc97989f2c521150
rgalina87 <r.galina87@gmail.com>
2020-05-04 12:24:20 +0300

    DC

---
+++ Week_3/Day_1/W3_D1_DC.js
@@ -0,0 +1,22 @@
+//first task//
+let array = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+array.shift()
+
+array.sort()
+
+array.push("Kiwi")
+
+array.shift()
+
+array.reverse()
+
+//second task//
+let array2 = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+
+console.log(array2[1][1])
+
+
+
+
+
