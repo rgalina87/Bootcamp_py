722c12015223b290d2607ff8396d0ef8017624d3
rgalina87 <r.galina87@gmail.com>
2020-05-08 17:10:48 +0300

    MP1

---
+++ Week_3/Day_5/W3_D5_XP.js
@@ -0,0 +1,31 @@
+//Exercise 1
+	let numbers= 5 + 25;
+		if (numbers < 100) {
+			console.log(true)
+		} else {
+			console.log(false)
+		}
+//Exercise 2
+
+	let numbers= 5 + 25;
+		console.log(5 % 25)
+	
+//Exercise 3
+		
+	let numbers = 5, 25
+		if (5/25 %2 ==0) {
+			console.log(true)
+		} else {
+			console.log(false)
+		}
+	
+//Exercise 4
+
+	//let mes= prompt('Write a number!');
+	let number= prompt('Enter a number!', ' ')
+		if (number <= '6') {
+			console.log(true)
+		} else {
+			console.log(false)
+		}
+	
