721aaa37da976d99fbe29b613fae7a476503891b
rgalina87 <r.galina87@gmail.com>
2020-05-17 17:09:15 +0300

    not finished

---
+++ Week_3/Day_5/W3_D5_XPGold.js
@@ -0,0 +1,52 @@
+//Exercise 1
+
+	let numbers= [0, -1, 4]
+	numbers.sort(function(a,b){
+		return b - a;
+	});
+	alert([4, 0, -1])
+
+//Exercise 2
+
+let numbers=  [3, -7, 2]
+let x=3;
+let y=-7;
+let z=2;
+if (x>0 && y>0 && z>0)
+{
+       alert("The sign is +");
+}
+else if (x<0 && y<0 && z<0)
+        {
+          console.log("The sign is +");
+        }
+        else if (x>0 && y<0 && z<0)
+        {
+          console.log("The sign is +");
+        }
+        else if (x<0 && y>0 && z<0)
+        {
+          console.log("The sign is +");
+        }
+        else
+        {
+          console.log("The sign is -");
+        }
+
+//Exercise 3
+
+let grades = {
+    david:  80,
+    vinoth: 77,
+    divya: 88,
+    ishitha: 95,
+    thomas: 68,
+    lea: 77
+}
+
+//Exercise 4
+	for (let i = 0; i < 11; i++) {
+		
+	}
+		console.log()
+//Exercise 5
---
+++ Week_4/Day_1/W4_D1_XP.js
@@ -0,0 +1,21 @@
+//Write a function checkBasket(), that asks the user
+ //for the item he wants and that lets him know 
+ //if it’s in the basket or not
+
+amazonBasket = {
+    glasses: 1,
+    books: 2,
+    floss: 100
+}
+
+
+function to_check(){
+	let good = prompt ("");
+	return good;
+}
+
+let user = to_check();
+
+function check_basket(good){
+	for(let x in )
+}
