7d81b02a5ce8ede88d18b51fce7421c5c28c3ff3
rgalina87 <r.galina87@gmail.com>
2020-06-22 17:24:00 +0300

    hw

--- Week_10/Day_1/W10_D1_DC.py
+++ Week_10/Day_1/W10_D1_DC.py
@@ -1,16 +1,15 @@
-from translate import Translator
+from googletrans import Translator
 
 def f():
+    translator = Translator()
     french_words = ["Bonjour", "Au revoir", "Bienvenue", "A bientôt"]
     transl = []
-
-    translator = Translator(from_lang="FR", to_lang="EN")
-    translation = translator.translate()
+    translation = translator.translate(french_words, src="FR", dest="EN")
 
     for x in translation:
         transl.append(x)
+    # return transl
 
     tr = {french_words[t]:transl[t] for t in range(4)}
-    return tr
-
-print(f())
+    print(tr)
+f()
